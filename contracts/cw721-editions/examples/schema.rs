b9a23c5191f62e8670117571ae4e7bfb49bcccfa
Vernon Johnson <vtj2105@columbia.edu>
2021-12-19 11:59:35 -0600

    fix: cw721-editions import for schemas

--- contracts/cw721-editions/examples/schema.rs
+++ contracts/cw721-editions/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw_editions::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw721_editions::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
