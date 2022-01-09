0ac4043c796c7b790b82043f1cce5d2968fb7361
Vernon Johnson <25010543+verminator23@users.noreply.github.com>
2022-01-09 14:20:34 -0600

    feat: add cw721-fixed-price contract (#36)
    * feat: add cw721 editions contract
    
    * fix: cw721-editions import for schemas
    
    * chore: add cw721-editions to circleci config
    
    * fix: circleci config.yml
    
    * lint: correct lint errors for cw721-editions contract
    
    * lint: fix remaining lint errors in cw721-editions
    
    * chore: rename cw721-editions -> cw721-fixed-price
    
    * chore: update Cargo.lock
    
    * fix: use library feature for cw-base dep in cw721-fixed-price contract
    
    * chore: update cw-utils to v0.11.1
    
    * chore: bump remaining cw-plus dependencies
    
    * chore: update Cargo.lock
    
    * fix: update cw721-base dep for cw721-fixed-price
    
    * fix: add config to example schema
    
    * fix: add config_response schema
    
    * fix: lint issues with example schema
    
    Co-authored-by: Orkun Külçe <orkunkl@users.noreply.github.com>

---
+++ contracts/cw721-fixed-price/src/msg.rs
@@ -0,0 +1,43 @@
+use cosmwasm_std::{Addr, Uint128};
+use cw721_base::Extension;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub owner: Addr,
+    pub max_tokens: u32,
+    pub unit_price: Uint128,
+    pub name: String,
+    pub symbol: String,
+    pub token_code_id: u64,
+    pub cw20_address: Addr,
+    pub token_uri: String,
+    pub extension: Extension,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Cw20ReceiveMsg { sender: String, amount: Uint128 },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetConfig {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ConfigResponse {
+    pub owner: Addr,
+    pub cw20_address: Addr,
+    pub cw721_address: Option<Addr>,
+    pub max_tokens: u32,
+    pub unit_price: Uint128,
+    pub name: String,
+    pub symbol: String,
+    pub token_uri: String,
+    pub extension: Extension,
+    pub unused_token_id: u32,
+}
---
+++ contracts/cw721-fixed-price/src/state.rs
@@ -0,0 +1,22 @@
+use cw721_base::Extension;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub owner: Addr,
+    pub cw20_address: Addr,
+    pub cw721_address: Option<Addr>,
+    pub max_tokens: u32,
+    pub unit_price: Uint128,
+    pub name: String,
+    pub symbol: String,
+    pub token_uri: String,
+    pub extension: Extension,
+    pub unused_token_id: u32,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
