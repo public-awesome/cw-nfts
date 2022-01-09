7d429bc11b6d6c5a2b0fca800d7defde97cc5927
Vernon Johnson <vtj2105@columbia.edu>
2022-01-09 14:13:26 -0600

    fix: add config to example schema

--- contracts/cw721-fixed-price/examples/schema.rs
+++ contracts/cw721-fixed-price/examples/schema.rs
@@ -3,7 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw721_fixed_price::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw721_fixed_price::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, ConfigResponse};
+
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,4 +15,6 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
 }
