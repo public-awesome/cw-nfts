4e26419bb02f4b871fda487964a80bd419207428
Michael J.S <32441941+humanalgorithm@users.noreply.github.com>
2022-05-27 09:00:23 -0400

    Fix TokenIndexes MultiIndex (#64)
    * updating to String index
    
    * fix lint and remove superfluous line

--- contracts/cw721-base/src/query.rs
+++ contracts/cw721-base/src/query.rs
@@ -160,7 +160,6 @@ where
             .prefix(owner_addr)
             .keys(deps.storage, start, None, Order::Ascending)
             .take(limit)
-            .map(|x| x.map(|addr| addr.to_string()))
             .collect::<StdResult<Vec<_>>>()?;
 
         Ok(TokensResponse { tokens })
