ecc42b53e56a3050f24d62fd4e3758d18dad21d4
Jake Hartnell <Jake.Hartnell@gmail.com>
2022-07-17 02:25:23 +0000

    Fix merge conflict issue

--- contracts/cw721-fixed-price/src/contract.rs
+++ contracts/cw721-fixed-price/src/contract.rs
@@ -175,7 +175,7 @@ mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info, MOCK_CONTRACT_ADDR};
     use cosmwasm_std::{
-        from_binary, to_binary, ContractResult, CosmosMsg, SubMsgExecutionResponse,
+        from_binary, to_binary, CosmosMsg, SubMsgExecutionResponse, SubMsgResult
     };
     use prost::Message;
 
