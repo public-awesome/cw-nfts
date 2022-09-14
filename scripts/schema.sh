9cf591f6972a0fb844f7b7e41001411b6b45b47f
Jake Hartnell <jake.hartnell@gmail.com>
2022-09-14 12:55:04 -0700

    Update schema, add schema generation script from DAO DAO

---
+++ scripts/schema.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+START_DIR=$(pwd)
+for f in ./contracts/*
+do
+  echo "generating schema for $f"
+  cd "$f"
+  CMD="cargo run --example schema"
+  eval $CMD > /dev/null
+  cd "$START_DIR"
+done
