51cbada0ad65ef77ba8751d288bbbff629084130
hoanm <mr.nmh175@gmail.com>
2022-11-02 11:08:43 +0700

    remove scripts for deployment, testing and add readme

--- scripts/node_modules/cosmjs-types/gogoproto/gogo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* eslint-disable */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protobufPackage = void 0;
-exports.protobufPackage = "gogoproto";
-//# sourceMappingURL=gogo.js.map
--- scripts/node_modules/cosmjs-types/google/api/annotations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* eslint-disable */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protobufPackage = void 0;
-exports.protobufPackage = "google.api";
-//# sourceMappingURL=annotations.js.map
