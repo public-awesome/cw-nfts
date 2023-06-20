54a435930dc33c6130f015f1ddb55929efa12d2c
Jake Hartnell <jake.hartnell@gmail.com>
2023-06-19 20:40:48 -0700

    Clean up, add some comments

--- contracts/cw721-sylvia-base/src/contract.rs
+++ contracts/cw721-sylvia-base/src/contract.rs
@@ -14,9 +14,14 @@ use sylvia::types::{ExecCtx, InstantiateCtx, QueryCtx};
 
 use crate::ContractError;
 
+// Version info for migration
+pub const CONTRACT_NAME: &str = "crates.io:cw721-sylvia-base";
+pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 const DEFAULT_LIMIT: u32 = 10;
 const MAX_LIMIT: u32 = 100;
 
+// TODO should this just be in cw721_interface along with the other responses?
 #[cw_serde]
 pub struct Approval {
     /// Account that can transfer/send the token
@@ -31,12 +36,15 @@ impl Approval {
     }
 }
 
+/// TODO move to a responses file?
 /// Shows who can mint these tokens
 #[cw_serde]
 pub struct MinterResponse {
     pub minter: Option<String>,
 }
 
+// TODO what do we want to do with extensions? They seem to be unnessary now?
+// TODO kill extensions
 #[cw_serde]
 pub struct TokenInfo {
     /// The owner of the newly minted NFT
@@ -107,7 +115,7 @@ impl Cw721Contract<'_> {
         ctx: InstantiateCtx,
         data: InstantiateMsgData,
     ) -> StdResult<Response> {
-        // TODO save contract metadata????
+        cw2::set_contract_version(ctx.deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
         let info = ContractInfoResponse {
             name: data.name,
@@ -177,6 +185,8 @@ impl Cw721Contract<'_> {
         cw_ownable::get_ownership(ctx.deps.storage)
     }
 
+    // TODO figure out the best place for methods like this...
+    // Love sylvia, but could use help figuring out good patterns
     pub fn increment_tokens(&self, storage: &mut dyn Storage) -> StdResult<u64> {
         let val = self.token_count.may_load(storage)?.unwrap_or_default() + 1;
         self.token_count.save(storage, &val)?;
@@ -197,7 +207,7 @@ impl Cw721Contract<'_> {
     ) -> Result<TokenInfo, ContractError> {
         let mut token = self.tokens.load(ctx.deps.storage, token_id)?;
         // ensure we have permissions
-        self.check_can_send(&ctx, &token)?;
+        self.check_can_send(ctx, &token)?;
         // set owner and remove existing approvals
         token.owner = ctx.deps.api.addr_validate(recipient)?;
         token.approvals = vec![];
@@ -217,7 +227,7 @@ impl Cw721Contract<'_> {
     ) -> Result<TokenInfo, ContractError> {
         let mut token = self.tokens.load(ctx.deps.storage, token_id)?;
         // ensure we have permissions
-        self.check_can_approve(&ctx, &token)?;
+        self.check_can_approve(ctx, &token)?;
 
         // update the approval list (remove any for the same spender before adding)
         let spender_addr = ctx.deps.api.addr_validate(spender)?;
@@ -297,6 +307,7 @@ impl Cw721Contract<'_> {
     }
 }
 
+// TODO break up into separte file? base.rs?
 #[contract]
 #[messages(cw721_interface as Cw721Interface)]
 impl Cw721Interface for Cw721Contract<'_> {
