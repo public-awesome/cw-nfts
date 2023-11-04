5256f3b73adecdf1d39f43c7f5fd0b3c5b95519e
mr-t <mr-t@arkprotocol.io>
2023-11-04 23:16:10 +0100

    prepare expiration and mint timestamps

--- contracts/cw721-expiration/src/execute.rs
+++ contracts/cw721-expiration/src/execute.rs
@@ -1,5 +1,6 @@
 use cosmwasm_std::{Binary, DepsMut, Empty, Env, MessageInfo, Response, StdResult};
 use cw721::{Cw721Execute, Expiration};
+use cw721_base::Cw721Contract;
 
 use crate::{state::Cw721ExpirationContract, ContractError, ExecuteMsg, Extension, InstantiateMsg};
 
@@ -21,7 +22,63 @@ impl<'a> Cw721ExpirationContract<'a> {
         info: MessageInfo,
         msg: ExecuteMsg,
     ) -> Result<Response, ContractError> {
-        self.base_contract.execute(deps, env, info, msg)
+        match msg {
+            ExecuteMsg::Mint {
+                token_id,
+                owner,
+                token_uri,
+                extension,
+            } => self.mint(deps, info, token_id, owner, token_uri, extension),
+            ExecuteMsg::Approve {
+                spender,
+                token_id,
+                expires,
+            } => self.approve(deps, env, info, spender, token_id, expires),
+            ExecuteMsg::Revoke { spender, token_id } => {
+                self.revoke(deps, env, info, spender, token_id)
+            }
+            ExecuteMsg::ApproveAll { operator, expires } => {
+                self.approve_all(deps, env, info, operator, expires)
+            }
+            ExecuteMsg::RevokeAll { operator } => self.revoke_all(deps, env, info, operator),
+            ExecuteMsg::TransferNft {
+                recipient,
+                token_id,
+            } => self.transfer_nft(deps, env, info, recipient, token_id),
+            ExecuteMsg::SendNft {
+                contract,
+                token_id,
+                msg,
+            } => self.send_nft(deps, env, info, contract, token_id, msg),
+            ExecuteMsg::Burn { token_id } => self.burn(deps, env, info, token_id),
+            ExecuteMsg::UpdateOwnership(action) => Self::update_ownership(deps, env, info, action),
+            ExecuteMsg::Extension { msg: _ } => Ok(Response::default()),
+        }
+    }
+
+}
+
+impl<'a> Cw721ExpirationContract<'a>
+{
+    pub fn mint(
+        &self,
+        deps: DepsMut,
+        info: MessageInfo,
+        token_id: String,
+        owner: String,
+        token_uri: Option<String>,
+        extension: Extension,
+    ) -> Result<Response, ContractError> {
+        self.base_contract.mint(deps, info, token_id, owner, token_uri, extension)
+    }
+
+    pub fn update_ownership(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        action: cw_ownable::Action,
+    ) -> Result<Response, ContractError> {
+        Cw721Contract::<Extension, Empty, Empty, Empty>::update_ownership(deps, env, info, action)
     }
 }
 
--- contracts/cw721-expiration/src/state.rs
+++ contracts/cw721-expiration/src/state.rs
@@ -1,12 +1,24 @@
-use cosmwasm_std::Empty;
+use cosmwasm_std::{Empty, Timestamp};
 use cw721::Cw721;
+use cw_storage_plus::{Item, Map};
 
 use crate::Extension;
 
-#[derive(Default)]
 pub struct Cw721ExpirationContract<'a> {
+    pub expiration_days: Item<'a, u64>,
+    pub mint_timestamps: Map<'a, &'a str, Timestamp>,
     pub base_contract: cw721_base::Cw721Contract<'a, Extension, Empty, Empty, Empty>,
 }
 
 // This is a signal, the implementations are in other files
 impl<'a> Cw721<Extension, Empty> for Cw721ExpirationContract<'a> {}
+
+impl Default for Cw721ExpirationContract<'static> {
+    fn default() -> Self {
+        Self {
+            expiration_days: Item::new("expiration_days"),
+            mint_timestamps: Map::new("mint_timestamps"),
+            base_contract: cw721_base::Cw721Contract::default(),
+        }
+    }
+}
