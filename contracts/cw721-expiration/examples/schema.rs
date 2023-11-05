960857ad900182a7d958eac524339bcd717fbbe6
mr-t <mr-t@arkprotocol.io>
2023-11-05 21:48:42 +0100

    cargo schema

---
+++ contracts/cw721-expiration/examples/schema.rs
@@ -0,0 +1,10 @@
+use cosmwasm_schema::write_api;
+use cw721_expiration::msg::{InstantiateMsg, ExecuteMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
