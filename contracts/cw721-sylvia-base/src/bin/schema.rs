9c6b8841d2c6c68cfda3f561fbf728b12d4cf070
Jake Hartnell <jake.hartnell@gmail.com>
2023-12-07 17:27:44 -0800

    Add schema

---
+++ contracts/cw721-sylvia-base/src/bin/schema.rs
@@ -0,0 +1,12 @@
+use cosmwasm_schema::write_api;
+
+use cw721_sylvia_base::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+
+#[cfg(not(tarpaulin_include))]
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ContractExecMsg,
+        query: ContractQueryMsg,
+    }
+}
