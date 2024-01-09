6b69f5ad6e819ffb13d253aa5415d47f60b32983
Christoph Otter <chris@confio.gmbh>
2024-01-09 12:09:32 +0100

    Add more test cases

--- contracts/cw721-receiver/tests/multitest.rs
+++ contracts/cw721-receiver/tests/multitest.rs
@@ -1,21 +1,121 @@
-use cosmwasm_std::{to_json_binary, Addr};
+use cosmwasm_std::{to_json_binary, Addr, Attribute, Binary};
 use cw_multi_test::{App, ContractWrapper, Executor};
 
 #[test]
-fn test_cw721_base_receive() {
+fn test_cw721_base_receive_succeed() {
+    use cw721_base::msg::*;
+    use cw721_receiver::msg::InnerMsg;
+
+    let mut app = App::default();
+    let admin = app.api().addr_make("admin");
+
+    let Contracts {
+        nft_contract,
+        receiver_contract,
+    } = setup_contracts(&mut app, admin.clone());
+
+    // send token to receiver contract
+    let response = app
+        .execute_contract(
+            admin.clone(),
+            nft_contract.clone(),
+            &ExecuteMsg::<(), ()>::SendNft {
+                contract: receiver_contract.to_string(),
+                token_id: "test".to_string(),
+                msg: to_json_binary(&InnerMsg::Succeed).unwrap(),
+            },
+            &[],
+        )
+        .unwrap();
+    let mut wasm_events = response.events.iter().filter(|e| e.ty == "wasm");
+
+    let send_event = wasm_events.next().unwrap();
+    assert_eq!(
+        get_attribute(&send_event.attributes, "action"),
+        Some("send_nft")
+    );
+    assert_eq!(
+        get_attribute(&send_event.attributes, "token_id"),
+        Some("test")
+    );
+    assert_eq!(
+        get_attribute(&send_event.attributes, "recipient"),
+        Some(receiver_contract.as_str())
+    );
+
+    let receive_event = wasm_events.next().unwrap();
+    assert_eq!(
+        get_attribute(&receive_event.attributes, "action"),
+        Some("receive_nft")
+    );
+    assert_eq!(
+        get_attribute(&receive_event.attributes, "token_id"),
+        Some("test")
+    );
+    assert_eq!(
+        get_attribute(&receive_event.attributes, "sender"),
+        Some(admin.as_str()) // this is set to the sender of the original message
+    );
+}
+
+#[test]
+fn test_cw721_base_receive_fail() {
+    use cw721_base::msg::*;
+    use cw721_receiver::msg::InnerMsg;
+
+    let mut app = App::default();
+    let admin = app.api().addr_make("admin");
+
+    let Contracts {
+        nft_contract,
+        receiver_contract,
+    } = setup_contracts(&mut app, admin.clone());
+
+    // send fail message
+    let result = app.execute_contract(
+        admin.clone(),
+        nft_contract.clone(),
+        &ExecuteMsg::<(), ()>::SendNft {
+            contract: receiver_contract.to_string(),
+            token_id: "test".to_string(),
+            msg: to_json_binary(&InnerMsg::Fail).unwrap(),
+        },
+        &[],
+    );
+    assert!(result.is_err());
+
+    // send incorrect message
+    let result = app.execute_contract(
+        admin,
+        nft_contract,
+        &ExecuteMsg::<(), ()>::SendNft {
+            contract: receiver_contract.to_string(),
+            token_id: "test".to_string(),
+            msg: Binary::from(br#"{"invalid": "fields"}"#),
+        },
+        &[],
+    );
+    assert!(result.is_err());
+}
+
+struct Contracts {
+    nft_contract: Addr,
+    receiver_contract: Addr,
+}
+
+/// Setup the cw721-receiver and cw721-base contracts and mint a test token
+fn setup_contracts(app: &mut App, admin: Addr) -> Contracts {
     use cw721_receiver::contract::*;
     use cw721_receiver::msg::*;
 
     use cw721_base::msg as base_msg;
 
-    let mut app = App::default();
     let code_id = app.store_code(Box::new(ContractWrapper::new(execute, instantiate, query)));
     let nft_code_id = app.store_code(Box::new(ContractWrapper::new(
         cw721_base::entry::execute,
         cw721_base::entry::instantiate,
         cw721_base::entry::query,
     )));
-    let admin = Addr::unchecked("admin");
 
     // setup contracts
     let nft_contract = app
@@ -58,17 +158,15 @@ fn test_cw721_base_receive() {
     )
     .unwrap();
 
-    // send token to receiver contract
-    let response = app
-        .execute_contract(
-            admin,
-            nft_contract,
-            &base_msg::ExecuteMsg::<(), ()>::SendNft {
-                contract: receiver_contract.to_string(),
-                token_id: "test".to_string(),
-                msg: to_json_binary(&InnerMsg::Succeed).unwrap(),
-            },
-            &[],
-        )
-        .unwrap();
+    Contracts {
+        nft_contract,
+        receiver_contract,
+    }
+}
+
+fn get_attribute<'a>(attributes: &'a [Attribute], key: &str) -> Option<&'a str> {
+    attributes
+        .iter()
+        .find(|a| a.key == key)
+        .map(|a| a.value.as_str())
 }
