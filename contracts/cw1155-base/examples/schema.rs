5875ad0ee864b028501f9d1528fd98f90eff354e
shab <dirtyshab@protonmail.com>
2024-05-03 15:19:38 -0400

    fix: schema files

--- contracts/cw1155-base/examples/schema.rs
+++ contracts/cw1155-base/examples/schema.rs
@@ -1,33 +1,15 @@
-use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
+use cosmwasm_schema::{remove_schemas, write_api};
+use cosmwasm_std::Empty;
 
-use cw1155::{
-    AllBalancesResponse, ApprovalsForResponse, BalanceResponse, BatchBalanceResponse,
-    Cw1155QueryMsg, MinterResponse, NumTokensResponse, TokenInfoResponse, TokensResponse,
-};
-use cw1155_base::{ExecuteMsg, Extension, InstantiateMsg};
+use cw1155::{Cw1155ExecuteMsg, Cw1155InstantiateMsg, Cw1155QueryMsg};
+use cw1155_base::Extension;
 
 fn main() {
-    let mut out_dir = current_dir().unwrap();
-    out_dir.push("schema");
-    create_dir_all(&out_dir).unwrap();
-    remove_schemas(&out_dir).unwrap();
-
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema_with_title(&schema_for!(ExecuteMsg<Extension>), &out_dir, "ExecuteMsg");
-    export_schema(&schema_for!(Cw1155QueryMsg), &out_dir);
-    export_schema(&schema_for!(BalanceResponse), &out_dir);
-    export_schema(&schema_for!(AllBalancesResponse), &out_dir);
-    export_schema(&schema_for!(BatchBalanceResponse), &out_dir);
-    export_schema(&schema_for!(NumTokensResponse), &out_dir);
-    export_schema(&schema_for!(ApprovalsForResponse), &out_dir);
-    export_schema(&schema_for!(TokensResponse), &out_dir);
-    export_schema(&schema_for!(MinterResponse), &out_dir);
-    export_schema_with_title(
-        &schema_for!(TokenInfoResponse<Extension>),
-        &out_dir,
-        "TokenInfoResponse",
-    );
+    write_api! {
+        instantiate: Cw1155InstantiateMsg,
+        execute: Cw1155ExecuteMsg<Extension>,
+        query: Cw1155QueryMsg<Empty>,
+    }
 }
--- packages/cw1155/examples/schema.rs
+++ packages/cw1155/examples/schema.rs
@@ -3,14 +3,14 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{remove_schemas, write_api};
 use cosmwasm_std::Empty;
 
-use cw1155::{Cw1155ExecuteMsg, Cw1155QueryMsg};
+use cw1155::{Cw1155ExecuteMsg, Cw1155InstantiateMsg, Cw1155QueryMsg};
 
 type Extension = Empty;
 
 fn main() {
     write_api! {
-        instantiate: InstantiateMsg,
-        execute: Cw1155ExecuteMsg,
-        query: Cw1155QueryMsg,
+        instantiate: Cw1155InstantiateMsg,
+        execute: Cw1155ExecuteMsg<Extension>,
+        query: Cw1155QueryMsg<Empty>,
     }
 }
