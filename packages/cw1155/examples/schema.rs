32c6e419b2ecfa05f50c4b0c72970cc5d1d6a96d
shab <dirtyshab@protonmail.com>
2024-05-23 14:56:32 -0400

    fix: cargo schema package

--- packages/cw1155/examples/schema.rs
+++ packages/cw1155/examples/schema.rs
@@ -3,12 +3,12 @@ use cosmwasm_std::Empty;
 
 use cw1155::{Cw1155ExecuteMsg, Cw1155InstantiateMsg, Cw1155QueryMsg};
 
-type Extension = Empty;
+type Extension = Option<Empty>;
 
 fn main() {
     write_api! {
         instantiate: Cw1155InstantiateMsg,
-        execute: Cw1155ExecuteMsg<Extension>,
+        execute: Cw1155ExecuteMsg<Extension, Empty>,
         query: Cw1155QueryMsg<Empty>,
     }
 }
