67522f78127f173674f88650f445ea27b855a23c
mr-t <mr-t@arkprotocol.io>
2024-05-23 19:31:50 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-## Compiles and optimizes contracts
+# Compiles and optimizes contracts
 
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-# build Ark proxies
 cd "$(git rev-parse --show-toplevel)"
 docker run --rm -v "$(pwd)":/code --platform linux/amd64 \
 	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
