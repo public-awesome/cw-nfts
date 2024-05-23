1d3b795aab31f75d3a0f4a9a036b484f20f9f0b7
shab <dirtyshab@protonmail.com>
2024-05-23 14:36:53 -0400

    fix: cargo schema

--- contracts/cw1155-base/examples/schema.rs
+++ contracts/cw1155-base/examples/schema.rs
@@ -1,13 +1,12 @@
 use cosmwasm_schema::write_api;
-use cosmwasm_std::Empty;
 
-use cw1155::{Cw1155ExecuteMsg, Cw1155InstantiateMsg, Cw1155QueryMsg};
-use cw1155_base::Extension;
+use cw1155::Cw1155InstantiateMsg;
+use cw1155_base::{Cw1155BaseExecuteMsg, Cw1155BaseQueryMsg};
 
 fn main() {
     write_api! {
         instantiate: Cw1155InstantiateMsg,
-        execute: Cw1155ExecuteMsg<Extension>,
-        query: Cw1155QueryMsg<Empty>,
+        execute: Cw1155BaseExecuteMsg,
+        query: Cw1155BaseQueryMsg,
     }
 }
--- contracts/cw1155-metadata-onchain/examples/schema.rs
+++ contracts/cw1155-metadata-onchain/examples/schema.rs
@@ -1,13 +1,13 @@
 use cosmwasm_schema::write_api;
 use cosmwasm_std::Empty;
 
-use cw1155::{Cw1155ExecuteMsg, Cw1155InstantiateMsg, Cw1155QueryMsg};
-use cw1155_metadata_onchain::Extension;
+use cw1155::{Cw1155InstantiateMsg, Cw1155QueryMsg};
+use cw1155_metadata_onchain::Cw1155MetadataExecuteMsg;
 
 fn main() {
     write_api! {
         instantiate: Cw1155InstantiateMsg,
-        execute: Cw1155ExecuteMsg<Extension>,
+        execute: Cw1155MetadataExecuteMsg,
         query: Cw1155QueryMsg<Empty>,
     }
 }
