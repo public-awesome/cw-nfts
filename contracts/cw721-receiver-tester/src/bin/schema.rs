fdfd28716c44baa74f97c1437d36a0018a5ce276
jhernandezb <contact@jhernandez.me>
2025-02-26 16:40:24 -0600

    fix schema generation

--- contracts/cw721-receiver-tester/src/bin/schema.rs
+++ contracts/cw721-receiver-tester/src/bin/schema.rs
@@ -1,11 +1,10 @@
 use cosmwasm_schema::write_api;
 
-use cw721_receiver_tester::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw721_receiver_tester::msg::{ExecuteMsg, InstantiateMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
-        query: QueryMsg,
     }
 }
